fdd148c9fe8b7422b8921691e5895ab20b666ca6
Rob Hinst <robert.hinst@crowncastle.com>
2020-12-31 18:23:43 -0500

    Fixed platform identification substring for RPi ARM CPU

--- sonar/gpio.py
+++ sonar/gpio.py
@@ -1,7 +1,7 @@
 import platform
 
 
-if platform.platform().lower().find("armv71") > -1:
+if platform.platform().lower().find("armv7l") > -1:
     import RPi.GPIO as GPIO
 else:
     import Mock.GPIO as GPIO
